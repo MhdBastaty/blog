81f5314ffdf1bdbda063cfeb154e9b773f3f3557
Mohammad Bastaty <72391469+MhdBastaty@users.noreply.github.com>
2021-05-23 21:54:35 +0800

    Add,Change : add a new route with new view

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+        <article>
+            <h1> <a href="/post">My First Post</a></h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut non nam vero i
+                psum sint, perspiciatis corporis. Eos, dolore modi fuga quasi labore possimus alias d
+                istinctio ex iste quos repellat. Lorem ipsum dolor sit amet, consectetur adipisic
+                ing elit. Pariatur, laudantium? Velit eius, impedit incidunt consequatur ipsam architecto! Porr
+                o voluptatibus quos 
+                maiores maxime repellendus earum debitis sint! Magni nesciunt voluptates consequatur!
+            </p>
+        </article>
+
+        <article>
+            <h1> <a href="/post">My Second Post</a></h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut non nam vero i
+                psum sint, perspiciatis corporis. Eos, dolore modi fuga quasi labore possimus alias d
+                istinctio ex iste quos repellat. Lorem ipsum dolor sit amet, consectetur adipisic
+                ing elit. Pariatur, laudantium? Velit eius, impedit incidunt consequatur ipsam architecto! Porr
+                o voluptatibus quos 
+                maiores maxime repellendus earum debitis sint! Magni nesciunt voluptates consequatur!
+            </p>
+        </article>
+
+        <article>
+            <h1> <a href="/post">My Third Post</a></h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut non nam vero i
+                psum sint, perspiciatis corporis. Eos, dolore modi fuga quasi labore possimus alias d
+                istinctio ex iste quos repellat. Lorem ipsum dolor sit amet, consectetur adipisic
+                ing elit. Pariatur, laudantium? Velit eius, impedit incidunt consequatur ipsam architecto! Porr
+                o voluptatibus quos 
+                maiores maxime repellendus earum debitis sint! Magni nesciunt voluptates consequatur!
+            </p>
+        </article>
+
+</body>
+</html>
